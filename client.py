ec14415e43d7195fd503727a25ea7fec98de87ea
rogermylife Hu <rogermylifeQ@gmail.com>
2016-07-20 18:03:46 +0800

    connect finished

--- client.py
+++ client.py
@@ -1,31 +1,93 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
 import socket
+import sys
+import os
+from threading import Thread
 
-#clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientSocket = socket.socket()
+ip = ''
+port = 9001
+BUFFER_SIZE = 1024
+s = None
+recvThread = None
 
-host = socket.gethostname()
-port = 5566
+class RecvThread(Thread):
 
-clientSocket.connect((host,port))
-clientSocket.send('Hello server!')
+    def __init__(self,ip,port,sock,name):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.sock = sock
+        self.name = name
+        #print " New thread started for "+ip+":"+str(port)
+        print 'successfully connect %s:%s' % (inputs[1],inputs[2])
 
-with open('received_file','wb') as received_file:
+    def run(self):
+        while True:
+            try:
+                data = self.sock.recv(BUFFER_SIZE)
+                data = data.split()
+            except:
+                print '%s:%s connection closed' % (self.ip,self.port)
+                break
+            print 'recv %s' % data
+    def kill(self):
+        print 'stop'
+        self.sock.close()
+        os.kill(os.getpid(),9)
+
+name = raw_input("Name:  ")
+
+
+while True:
+    line = sys.stdin.readline()
+    inputs = line.split()
+    if not inputs:
+        continue
+    
+    
+    elif inputs[0]=='connect' :
+        if len(inputs) !=3:
+            print 'connect command error'
+            continue
+        print 'connecting...'
+        if s != None:
+            #print 'close ex'
+            s.send("exit()")
+            s.close()
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((inputs[1],int(inputs[2])))
+            ip = inputs[1]
+            port = int(inputs[2])
+            s.send(name)
+            recvThread = RecvThread(ip,port,s,name)
+            recvThread.start()
+        except socket.error,msg:
+            print 'socket error %s' % msg
+
+    
+    elif inputs[0]=='exit()':
+        recvThread.kill()
+    
+    
+    else :
+        print 'no this command'
+
+s.connect((TCP_IP, TCP_PORT))
+with open('received_file', 'wb') as f:
     print 'file opened'
     while True:
-        print('receiving data...')
-        data = clientSocket.recv(1024)
-        print('data=%s' % (data))
+        #print('receiving data...')
+        data = s.recv(BUFFER_SIZE)
+        print('data=%s', (data))
         if not data:
+            f.close()
+            print 'file close()'
             break
-        received_file.write(data)
+        # write data to a file
+        f.write(data)
 
-received_file.close()
 print('Successfully get the file')
-#tm = clientSocket.recv(1024)
-#tm = '天ㄚ  寫的完嘛?'
-
-clientSocket.close()
-
-print("connection closed" )
-#print("The time got from the server is %s and My host is %s" % (tm.decode('utf-8'),str(host)) )
+s.close()
+print('connection closed')
--- server.py
+++ server.py
@@ -1,1 +1,67 @@
-:wq
+import socket
+import time
+from threading import Thread
+from SocketServer import ThreadingMixIn
+
+
+TCP_IP = '188.166.241.205'
+TCP_PORT = 9001
+BUFFER_SIZE = 1024
+
+class ClientThread(Thread):
+
+    def __init__(self,ip,port,sock,name):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.sock = sock
+        self.name = name
+        print "[login] "+name+" from "+ip+":"+str(port)
+
+    def run(self):
+        while True:
+            try:
+                data = self.sock.recv(BUFFER_SIZE)
+                data = data.split()
+                if data[0]=='exit()' and len(data) == 1:
+                    print '%s from %s:%s connection closed' % (self.name,self.ip,self.port)
+                    self.sock.close()
+                    break
+                else:
+                    print data
+            except IndexError:
+                print '%s from %s:%s connection closed' % (self.name,self.ip,self.port)
+                break;
+            except:
+                print 'other failed'
+                break;
+        # filename='myText.txt'
+        # f = open(filename,'rb')
+        # while True:
+            # l = f.read(BUFFER_SIZE)
+            # while (l):
+                # self.sock.send(l)
+                #print('Sent ',repr(l))
+                # l = f.read(BUFFER_SIZE)
+            # if not l:
+                # f.close()
+                # self.sock.close()
+                # break
+
+tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcpsock.bind((TCP_IP, TCP_PORT))
+threads = []
+
+while True:
+    tcpsock.listen(5)
+    print "Waiting for incoming connections..."
+    (conn, (ip,port)) = tcpsock.accept()
+    print 'Got connection from ', (ip,port)
+    name = conn.recv(BUFFER_SIZE)
+    newthread = ClientThread(ip,port,conn,name)
+    newthread.start()
+    threads.append(newthread)
+
+for t in threads:
+    t.join()
